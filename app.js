e13022ece5b03b501cf9c202ff003129244c2677
ubaidullah13august <abdulkhan1311@gmail.com>
2024-03-04 15:23:51 +0500

    assigment no 18 & 19

---
+++ app.js
@@ -0,0 +1,32 @@
+// q no 1
+var studentname=[""]
+// q no 2
+var studentname=[""]
+// q no 3
+var arry1 =["ubaid ullah"]
+console.log(arry1)
+// q no 4
+var arry2=[23,24,25]
+console.log(arry2)
+// q no 5
+var arry3=[true,false]
+console.log(arry3)
+// q no 6
+var arry4=["ubaid",23,true]
+console.log(arry4)
+// q no 7
+var arry5=[1 +"SSC"+"</br>"+2+"HSC"+"</br>" +3+"BCS"+"</br>" +4+"BS" +"</br>"+5 +"BCOM"+"</br>" +6+"MS"+"</br>" +7+"M"+"</br>" +8+"M.Phil."+"</br>" +9+"PHD"+"</br>"]
+document.write(arry5)
+"</br>"
+"</br>"
+"</br>"
+// q no 8
+var studentname1="michael"
+var studentname2="jhon"
+var studentname3="Tony"
+var percentage1=64
+var percentage2=46
+var percentage3=96
+document.write("score of ",studentname1," is ",320+"and percentage is"+percentage1+"</br>")
+document.write("score of ",studentname2," is ",230+"and percentage is"+percentage2+"</br>")
+document.write("score of ",studentname3," is ",480+"and percentage is"+percentage3+"</br>")
